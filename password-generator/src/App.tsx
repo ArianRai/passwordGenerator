b5c6db6711c0c1db567bbb0faea416e38fe50253
Arian <arianraiesteban_20@hotmail.com>
2023-09-22 20:52:56 +0200

    vite project start

---
+++ password-generator/src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <div className="main">
+		<form action="">
+			<label>Output
+			<input type="text" name="output"/>
+			</label>
+			<label htmlFor="lenght">Length</label>
+			<input type="range" name="length" id="" />
+			<input type="checkbox" name="" id="" />
+			<input type="checkbox" name="" id="" />
+			<input type="checkbox" name="" id="" />
+			<input type="submit" value="Aiuda" />
+		</form>
+	 </div>
+    </>
+  )
+}
+
+export default App
